26ab5166443fef823ad94419550a5b2a4b24bfe8
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-01-05 05:06:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     user = User.find_by(:username => params[:username])
-    if user && user.
+      if user && user.authenticate(params[:password])
       redirect "/account"
     else
       redirect "/failure"
